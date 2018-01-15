1041a5399d6acedc9fe13009e9bcb66eec24b369
nicholasc <nicholasc@alleninstitute.org>
2018-01-15 03:00:46 +0000

    bug fix

--- {{cookiecutter.project_slug}}/deploy.sh
+++ {{cookiecutter.project_slug}}/deploy.sh
@@ -19,5 +19,5 @@ helm install jupyterhub/binderhub --version=v0.1.0-397eb59 --name=binder --names
 
 # Now that JupyterHub, grab its IP address and update Binderhub to link together:
 jupyterhub_ip=`kubectl --namespace=binder get svc proxy-public | awk '{ print $4}' | tail -n 1`
-python create-config.py --id=$id --prefix=$prefix --jupyterhub_ip=$jupyterhub_ip $FORCE
+python create-config.py --id=$id --prefix=$prefix --jupyterhub_ip=$jupyterhub_ip --force
 helm upgrade binder jupyterhub/binderhub --version=v0.1.0-397eb59 -f secret.yaml -f config.yaml
