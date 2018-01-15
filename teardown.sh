6483d1901a3c5c6484bb3eb761875064a05706e2
nicholasc <nicholasc@alleninstitute.org>
2018-01-15 01:35:25 +0000

    read project id from env

--- teardown.sh
+++ teardown.sh
@@ -1,14 +1,13 @@
 zone='us-central1-b'
 cluster_name='hello-world'
-id='reliable-cairn-192018'
 
 helm delete binder --purge
 kubectl delete namespace binder
 gcloud container clusters delete $cluster_name --zone=$zone
 
 echo "Double-check resources are down:"
-echo "    https://console.cloud.google.com/compute/disks?project=$id"
-echo "    https://console.cloud.google.com/kubernetes/list?project=$id"
-echo "    https://console.cloud.google.com/gcr/images/$id?project=$id"
-echo "    https://console.cloud.google.com/net-services/loadbalancing/loadBalancers/list?project=$id"
+echo "    https://console.cloud.google.com/compute/disks?project=${DEVSHELL_PROJECT_ID:-Cloud Shell}"
+echo "    https://console.cloud.google.com/kubernetes/list?project=${DEVSHELL_PROJECT_ID:-Cloud Shell}"
+echo "    https://console.cloud.google.com/gcr/images/${DEVSHELL_PROJECT_ID:-Cloud Shell}?project=${DEVSHELL_PROJECT_ID:-Cloud Shell}"
+echo "    https://console.cloud.google.com/net-services/loadbalancing/loadBalancers/list?project=${DEVSHELL_PROJECT_ID:-Cloud Shell}"
 echo "For more info: https://zero-to-jupyterhub.readthedocs.io/en/latest/turn-off.html"
